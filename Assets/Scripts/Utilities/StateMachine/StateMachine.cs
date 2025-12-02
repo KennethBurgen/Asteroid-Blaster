89d39fbaf32189f33ed2b352d59f78bf9d42b41c
KennethBurgen <147069208+KennethBurgen@users.noreply.github.com>
2025-12-02 22:30:42 +0100

    StateMachine klasse erstellt

---
+++ Assets/Scripts/Utilities/StateMachine/StateMachine.cs
@@ -0,0 +1,24 @@
+using Utilities.StateMachine.Interfaces;
+
+namespace Utilities.StateMachine
+{
+    public class StateMachine
+    {
+        private IState _currentState;
+
+        public void ChangeState(IState newState)
+        {
+            _currentState?.OnExit();
+            _currentState = newState;
+            _currentState?.OnEnter();
+        }
+
+        public void Update()
+        {
+            if (_currentState is IUpdatableState updatableState)
+            {
+                updatableState.OnUpdate();
+            }
+        }
+    }
+}
