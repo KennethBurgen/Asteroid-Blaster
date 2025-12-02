a5e437de67b6db1c7b2005c0a0919b748b8c082c
KennethBurgen <147069208+KennethBurgen@users.noreply.github.com>
2025-12-02 21:29:06 +0100

    InputProvider zu Singleton gemacht

--- Assets/Scripts/Utilities/InputProvider.cs
+++ Assets/Scripts/Utilities/InputProvider.cs
@@ -4,6 +4,7 @@ namespace Utilities
 {
     public class InputProvider : MonoBehaviour
     {
+        private static InputProvider _instance;
         private PlayerInputActions _playerInputActions;
 
         public Vector2 MoveInput =>
@@ -13,8 +14,33 @@ namespace Utilities
         public bool SubmitInput =>
             _playerInputActions?.Player.Submit.WasPressedThisFrame() ?? false;
 
+        #region Singleton
+
+        /// <summary>
+        /// Stellt sicher, dass der InputProvider nur einmal existiert - Singleton
+        /// </summary>
+        private void InitializeSingleton()
+        {
+            if (!_instance && _instance != this)
+            {
+                Debug.LogWarning(
+                    "Deleting Object -"
+                        + gameObject.name
+                        + "- because Component InputProvider already exists."
+                );
+                Destroy(gameObject);
+                return;
+            }
+
+            _instance = this;
+        }
+
+        #endregion
+
         private void OnEnable()
         {
+            InitializeSingleton();
+
             _playerInputActions ??= new PlayerInputActions();
             _playerInputActions.Enable();
         }
