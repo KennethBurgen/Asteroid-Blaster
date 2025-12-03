37ef170be5a7cf1cb25587349853aa5fd25ce555
KennethBurgen <147069208+KennethBurgen@users.noreply.github.com>
2025-12-03 18:11:30 +0100

    TestOrdner für StateMachine auch renamed

--- Assets/Tests/EditMode/Utilities/StateMachineSystem/StateMachineTest.cs
+++ Assets/Tests/EditMode/Utilities/StateMachineSystem/StateMachineTest.cs
@@ -1,27 +1,18 @@
 using System.Collections;
+using Moq;
 using NUnit.Framework;
 using UnityEngine.TestTools;
-using Utilities.StateMachine.Interfaces;
+using Utilities.StateMachineSystem;
+using Utilities.StateMachineSystem.Interfaces;
 
 namespace Tests.EditMode.Utilities.StateMachine
 {
     public class StateMachineTest
     {
-        // A Test behaves as an ordinary method
         [Test]
         public void StateMachineTestSimplePasses()
         {
             // Use the Assert class to test conditions
-        }
-
-        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-        // `yield return null;` to skip a frame.
-        [UnityTest]
-        public IEnumerator StateMachineTestWithEnumeratorPasses()
-        {
-            // Use the Assert class to test conditions.
-            // Use yield to skip a frame.
-            yield return null;
         }
 
         private class ExampleUpdatableState : IUpdatableState
