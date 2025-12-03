d33e1d099b2c71788e09d48f39bd067370e3e03e
KennethBurgen <147069208+KennethBurgen@users.noreply.github.com>
2025-12-03 20:18:46 +0100

    Leere GameStates für MainMenu, GameOver und Playing erstellt

---
+++ Assets/Scripts/Core/GameManagerSystem/GameStates/GameOverGameState.cs
@@ -0,0 +1,17 @@
+using Utilities.StateMachineSystem.Interfaces;
+
+namespace Core.GameManagerSystem.GameStates
+{
+    public class GameOverGameState : IState
+    {
+        public void OnEnter()
+        {
+            // to be done
+        }
+
+        public void OnExit()
+        {
+            // to be done
+        }
+    }
+}
---
+++ Assets/Scripts/Core/GameManagerSystem/GameStates/MainMenuGameState.cs
@@ -0,0 +1,17 @@
+using Utilities.StateMachineSystem.Interfaces;
+
+namespace Core.GameManagerSystem.GameStates
+{
+    public class MainMenuGameState : IState
+    {
+        public void OnEnter()
+        {
+            // To be done
+        }
+
+        public void OnExit()
+        {
+            // To be done
+        }
+    }
+}
