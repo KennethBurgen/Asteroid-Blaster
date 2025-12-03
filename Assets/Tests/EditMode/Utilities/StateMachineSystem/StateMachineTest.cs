a737610e63e4b7b670681b536c40f7803be61758
KennethBurgen <147069208+KennethBurgen@users.noreply.github.com>
2025-12-03 18:38:14 +0100

    StateMachine Tests erstellt

--- Assets/Tests/EditMode/Utilities/StateMachineSystem/StateMachineTest.cs
+++ Assets/Tests/EditMode/Utilities/StateMachineSystem/StateMachineTest.cs
@@ -9,20 +9,53 @@ namespace Tests.EditMode.Utilities.StateMachineSystem
 {
     public class StateMachineTest
     {
-        [Test]
-        public void StateMachineTestSimplePasses()
+        private StateMachine _stateMachine;
+
+        [SetUp]
+        public void Setup()
         {
-            // Use the Assert class to test conditions
+            _stateMachine = new StateMachine();
         }
 
-        private class ExampleUpdatableState : IUpdatableState
+        [TearDown]
+        public void TearDown()
         {
-            // ACHTUNG: Hier wahrscheinlich sinnvoller moq-Framework zu implementieren und zu verwenden
-            public void OnEnter() { }
+            _stateMachine = null;
+        }
 
-            public void OnExit() { }
+        [Test]
+        public void CanChangeState()
+        {
+            var mockState = new Mock<IState>();
+            var mockState2 = new Mock<IState>();
 
-            public void OnUpdate() { }
+            _stateMachine.ChangeState(mockState.Object);
+            _stateMachine.ChangeState(mockState2.Object);
+
+            mockState.Verify(mock => mock.OnEnter(), Times.Once);
+            mockState.Verify(mock => mock.OnExit(), Times.Once);
+            mockState2.Verify(mock => mock.OnEnter(), Times.Once);
+        }
+
+        [Test]
+        public void CanDoAllTypesOfUpdatesOnUpdatableStates()
+        {
+            var mockState = new Mock<IUpdatableState>();
+            var mockState2 = new Mock<IFixedUpdatableState>();
+
+            _stateMachine.ChangeState(mockState.Object);
+
+            _stateMachine.Update();
+
+            _stateMachine.ChangeState(mockState2.Object);
+
+            _stateMachine.Update();
+            _stateMachine.FixedUpdate();
+
+            // Verifies
+            mockState.Verify(mock => mock.OnUpdate(), Times.Once);
+            mockState2.Verify(mock => mock.OnUpdate(), Times.Once);
+            mockState2.Verify(mock => mock.OnFixedUpdate(), Times.Once);
         }
     }
 }
