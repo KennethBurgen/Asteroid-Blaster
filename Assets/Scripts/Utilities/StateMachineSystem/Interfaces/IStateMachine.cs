d4e9aba2df3e923402f6e1e064e457215b588645
KennethBurgen <147069208+KennethBurgen@users.noreply.github.com>
2025-12-03 22:27:50 +0100

    Getter für CurrentState in IStateMachine aufgenommen

--- Assets/Scripts/Utilities/StateMachineSystem/Interfaces/IStateMachine.cs
+++ Assets/Scripts/Utilities/StateMachineSystem/Interfaces/IStateMachine.cs
@@ -2,6 +2,7 @@ namespace Utilities.StateMachineSystem.Interfaces
 {
     public interface IStateMachine
     {
+        public IState CurrentState { get; }
         public void ChangeState(IState newState);
         public void Update();
         public void FixedUpdate();
