323587fbaaaa6907062cdae4ab14cc0a56abc29a
KennethBurgen <147069208+KennethBurgen@users.noreply.github.com>
2025-12-03 21:09:56 +0100

    ChangeGameState und setSeed funktionalität eingebaut

--- Assets/Scripts/Core/GameManagerSystem/GameManager.cs
+++ Assets/Scripts/Core/GameManagerSystem/GameManager.cs
@@ -1,4 +1,7 @@
+using System;
+using Core.GameManagerSystem.GameStates;
 using Utilities.StateMachineSystem;
+using Utilities.StateMachineSystem.Interfaces;
 
 namespace Core.GameManagerSystem
 {
@@ -24,19 +27,44 @@ namespace Core.GameManagerSystem
         #endregion
 
         // Referenzen
-        private StateMachine _stateMachine;
+        private readonly StateMachine _stateMachine;
+        public IState CurrentGameState => _stateMachine.CurrentState;
+
+        // GameStates
+        public MainMenuGameState MainMenuGameState { get; private set; }
+        public GameOverGameState GameOverGameState { get; private set; }
+        public PlayingGameState PlayingGameState { get; private set; }
 
         // Fields
         private int _seed;
-        public int SEED
-        {
-            get => _seed;
-            set => _seed = value;
-        }
+        private int _minSeedLength;
+        public int SEED => _seed;
+
+        // Events
+        public event Action<IState> OnGameStateChanged;
 
         private GameManager()
         {
             _stateMachine = new StateMachine();
+            // GameStates
+            MainMenuGameState = new MainMenuGameState();
+            GameOverGameState = new GameOverGameState();
+            PlayingGameState = new PlayingGameState();
+        }
+
+        public void ChangeGameState(IState newGameState)
+        {
+            _stateMachine.ChangeState(newGameState);
+            OnGameStateChanged?.Invoke(newGameState);
+        }
+
+        public bool SetSeedSuccessful(int seed)
+        {
+            if (Math.Abs(seed).ToString().Length < _minSeedLength && seed == 0)
+                return false;
+
+            _seed = seed;
+            return true;
         }
     }
 }
