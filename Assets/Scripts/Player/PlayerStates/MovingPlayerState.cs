3cc51077d37725ed4411be069b2aff3fe9644710
KennethBurgen <147069208+KennethBurgen@users.noreply.github.com>
2025-12-05 16:27:53 +0100

    Erste leere PlayerStates hinzugefügt

---
+++ Assets/Scripts/Player/PlayerStates/MovingPlayerState.cs
@@ -0,0 +1,27 @@
+using Utilities.StateMachineSystem.Interfaces;
+
+namespace Player.PlayerStates
+{
+    public class MovingPlayerState : IFixedUpdatableState
+    {
+        public void OnEnter()
+        {
+            // to be done
+        }
+
+        public void OnExit()
+        {
+            // to be done
+        }
+
+        public void OnUpdate()
+        {
+            // to be done
+        }
+
+        public void OnFixedUpdate()
+        {
+            // to be done
+        }
+    }
+}
