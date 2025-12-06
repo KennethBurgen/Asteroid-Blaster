51ea14685a437aa23ac77da1fbe7796578d70e01
KennethBurgen <147069208+KennethBurgen@users.noreply.github.com>
2025-12-06 16:57:07 +0100

    IdlePlayerStateTest Tests hinzugefügt

--- Assets/Tests/EditMode/Player/PlayerStates/IdlePlayerStateTest.cs
+++ Assets/Tests/EditMode/Player/PlayerStates/IdlePlayerStateTest.cs
@@ -1,27 +1,66 @@
-using System.Collections;
+using Moq;
 using NUnit.Framework;
+using Player.Interfaces;
+using Player.PlayerStates;
 using UnityEngine;
-using UnityEngine.TestTools;
+using Utilities.Interfaces;
 
 namespace Tests.EditMode.Player.PlayerStates
 {
     public class IdlePlayerStateTest
     {
-        // A Test behaves as an ordinary method
-        [Test]
-        public void IdlePlayerStateTestSimplePasses()
+        private Mock<IPlayerManager> _playerManager;
+        private Mock<IPlayerController> _playerController;
+        private Mock<IInputProvider> _inputProvider;
+
+        private IdlePlayerState _sut;
+
+        [SetUp]
+        public void Setup()
         {
-            // Use the Assert class to test conditions
+            _playerManager = new Mock<IPlayerManager>();
+            _playerController = new Mock<IPlayerController>();
+            _inputProvider = new Mock<IInputProvider>();
+
+            _sut = new IdlePlayerState(_playerManager.Object, _playerController.Object)
+            {
+                _inputProvider = _inputProvider.Object,
+            };
         }
 
-        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-        // `yield return null;` to skip a frame.
-        [UnityTest]
-        public IEnumerator IdlePlayerStateTestWithEnumeratorPasses()
+        [TearDown]
+        public void TearDown()
         {
-            // Use the Assert class to test conditions.
-            // Use yield to skip a frame.
-            yield return null;
+            _sut = null;
+            _playerManager = null;
+            _playerController = null;
+        }
+
+        [Test]
+        public void OnEnterOk()
+        {
+            _sut.OnEnter();
+
+            _playerController.Verify(mock => mock.MovePlayer(Vector2.zero));
+        }
+
+        [Test]
+        public void CheckTransitionTransitionsToMovingPlayerState()
+        {
+            var movingPlayerState = new MovingPlayerState(
+                _playerManager.Object,
+                _playerController.Object
+            );
+
+            var vector2 = new Vector2(1, 0);
+
+            _inputProvider.Setup(mock => mock.MoveInput).Returns(vector2);
+            _playerManager.Setup(mock => mock.MovingPlayerState).Returns(movingPlayerState);
+
+            _sut.CheckTransition();
+
+            _playerManager.Verify(mock => mock.ChangePlayerState(movingPlayerState), Times.Once);
+            _playerController.Verify(mock => mock.MovePlayer(vector2), Times.Once);
         }
     }
 }
