7a2e370f70a07f7861cd5dd0db296a4163a06082
KennethBurgen <147069208+KennethBurgen@users.noreply.github.com>
2025-12-06 14:34:06 +0100

    Attribute werden in Manager zugewiesen

--- Assets/Scripts/Player/PlayerAttributes/PlayerAttributeManager.cs
+++ Assets/Scripts/Player/PlayerAttributes/PlayerAttributeManager.cs
@@ -1,19 +1,26 @@
-using UnityEngine;
-
 namespace Player.PlayerAttributes
 {
     public class PlayerAttributesManager
     {
         // Referenzen
-        private ScriptableObject _playerStats;
+        private readonly PlayerAttributes _playerStats;
 
         // PlayerStats
         public float HorizontalMovementSpeed { get; private set; }
         public int MaxHealth { get; private set; }
 
-        public PlayerAttributesManager(ScriptableObject playerStats)
+        public PlayerAttributesManager(PlayerAttributes playerStats)
         {
             _playerStats = playerStats;
+            SetAttributes();
+        }
+
+        private void SetAttributes()
+        {
+            // Resources
+            MaxHealth = _playerStats.MaxHealth;
+            // Movement
+            HorizontalMovementSpeed = _playerStats.HorizontalMovementSpeed;
         }
     }
 }
